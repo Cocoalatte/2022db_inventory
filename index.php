ae0ba34a4491f4b8222851db81e128939279aabe
Cocoalatte <github@chitose-tech.com>
2022-12-18 03:00:51 +0900

    switch to dynamic navbar

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php require("env.php");?>
+<?php require("env.php");
+
+
+
+
+?>
 <!DOCTYPE HTML>
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -8,27 +13,10 @@
     <title><?php print(APP_NAME);?></title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><?php print(APP_NAME);?></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-
-    </div>
-</nav>
+<?php generate_navbar(basename(__FILE__))?>
     <div class="container">
-        <?php
-        print("うんこ！");
-        ?>
+        <h1 class="page-header">Welcome</h1><hr>
+        上記メニュバーから操作を選んでください。
     </div>
 
 </body>
