4b192fb9a55f6718b328887e5f2266ee236e096c
Cocoalatte <github@chitose-tech.com>
2022-12-19 01:31:14 +0900

    add error_reproting

--- env.php
+++ env.php
@@ -1,8 +1,14 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+#共通処理&環境変数
+#
+#
+
 #env
 define("APP_NAME","Inventory");
 define("DB_FILE","inventory.db");
 
+#for dynamic navbar
 $inventory_navbar['0']['script'] = "index.php";
 $inventory_navbar['0']['name'] = "Home";
 $inventory_navbar['1']['script'] = "edit.php";
@@ -10,13 +16,13 @@ $inventory_navbar['1']['name'] = "新規物品登録";
 $inventory_navbar['2']['script'] = "list.php";
 $inventory_navbar['2']['name'] = "物品一覧";
 
-#MSG
-define("MSG001","DBのアクセスに失敗しました。");
 
 
 #common
+
+#cocoroconect
 function sqlite_connect(){
-    $db = sqlite_open(DB_FILE, 0666, $sqliteerror);
+    $db = sqlite_open(DB_FILE, 0666);
     if (!$db) {
         #fault
         return false;
@@ -25,8 +31,13 @@ function sqlite_connect(){
     }
 }
 
+#cocoroshutout
+
+
+#dynamic~~chord~~ navbar
 function generate_navbar($script_name){
     global $inventory_navbar;
+
     print '<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">'.APP_NAME.'</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,6 +45,7 @@ function generate_navbar($script_name){
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">';
+
     foreach($inventory_navbar as $nav_item){
         if($script_name == $nav_item["script"]){
             $nav_item_class = 'nav-item active';
