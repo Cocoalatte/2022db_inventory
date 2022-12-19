38248eb66488f750df4a2798b67993816f4fe182
Cocoalatte <github@chitose-tech.com>
2022-12-19 17:01:18 +0900

    add form dynamic generate

--- edit.php
+++ edit.php
@@ -1,105 +1,57 @@
 <?php
 
     require("env.php");
+    global $inventory_alert,$inventory_isfixed;
+    $dbhandle = new SQLite3(DB_FILE);
+    #
     if($_GET['eid'] != ""){
+        $result = $dbhandle->querySingle("SELECT * from inventory WHERE inventory_id = ".$_GET["eid"].";",true);
+
+        if(empty($result)){
+            $inventory_alert = MSG_ASSET_NOT_FOUND;#notfound
+        }
 
     }else{
+        $result = $dbhandle -> querySingle("SELECT MAX(id) FROM inventory;");
+        if($result != NULL){
+            $result['inventory_id'] ++;
+        }else{
+            $result['inventory_id'] = date("Y")*100000+1;
+        }
 
     }
-
-function combobox($item,$seleteced = null){
-        for($i = 0;$i <= array_key_last($item);$i++){
-            print ' <label for="inventory_status" class="col-form-label">ステータス</label>
-                <select class="custom-select" id="inventory_status">';
-        }
-}
+    $status_items = db_to_array($dbhandle->query("SELECT * FROM inventory_status;"));
+    $category_items = db_to_array($dbhandle->query("SELECT * FROM inventory_category;"));
+    $budget_items = db_to_array($dbhandle->query("SELECT * FROM inventory_budget;"));
 
 
+
+
+
+
+    #maketitle
+    generate_html_header(basename(__FILE__));
 ?>
-<!DOCTYPE HTML>
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <title>台帳編集 - <?php print(APP_NAME);?></title>
-</head>
-<body>
-<?php generate_navbar(basename(__FILE__))?>
-<!--
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><?php print(APP_NAME);?></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-        </ul>
-    </div>
-</nav>
--->
     <div class="container">
         <!-- -->
         <h1 class="page-header">物品詳細・編集</h1>
-
+        <?php print($inventory_alert);?>
         <form>
-            <div class="form-group">
-                <label for="inventory_id" class="col-form-label">物品ID(自動)</label>
-                <input type="text" class="form-control" id="inventory_id" readonly>
-            </div>
-            <div class="form-group">
-                <label for="inventory_name" class="col-form-label">物品名</label>
-                <input type="text" class="form-control" id="inventory_name">
-            </div>
-            <div class="form-group">
-                <label for="inventory_status" class="col-form-label">ステータス</label>
-                <select class="custom-select" id="inventory_status">
-                    <option value="1">在庫</option>
-                    <option value="2">注文中</option>
-                    <option value="3">修理中</option>
-                    <option value="4">捜索中</option>
-                    <option value="5">除却</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="inventory_alias" class="col-form-label">大学資産番号</label>
-                <input type="text" class="form-control" id="inventory_alias">
-            </div>
-            <div class="form-group">
-                <label for="inventory_location" class="col-form-label">保管場所</label>
-                <input type="text" class="form-control" id="inventory_location">
-            </div>
-            <div class="form-group">
-                <label for="inventory_category" class="col-form-label">カテゴリ</label>
-                <select class="custom-select" id="inventory_category">
-                    <option value="1">PC</option>
-                    <option value="2">家電・ガジェット</option>
-                    <option value="3">書籍</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="inventory_add_date" class="col-form-label">購入年月</label>
-                <input type="text" class="form-control" id="inventory_add_date">
-            </div>
-            <label for="inventory_budget" class="col-form-label">利用予算</label>
-            <select class="custom-select" id="inventory_budget">
-                <option value="0">未登録</option>
-                <option value="1">実験実習費</option>
-            </select>
-            <div class="form-group">
-                <label for="inventory_is_fixed_asset" class="col-form-label">動産/固定資産</label>
-                <select class="custom-select" id="inventory_is_fixed_asset">
-                    <option value="1">動産</option>
-                    <option value="2">固定資産</option>
-                </select>
-            </div>
+
+
+
+            <?php
+            forms_textbox("inventory_id","物品ID","","自動採番されます",true);
+            forms_textbox("inventory_name","物品名","");
+            forms_combobox("inventory_status","ステータス",$status_items);
+            forms_textbox("inventory_alias","大学資産番号","");
+            forms_textbox("inventory_location","保管場所","");
+            forms_combobox("inventory_category","カテゴリ",$category_items);
+            forms_textbox("inventory_add_date","購入年月日","");
+            forms_combobox("inventory_budget","予算区分",$budget_items);
+            forms_combobox("inventory_is_fixed","動産/固定資産",$inventory_isfixed);
+            forms_textarea("inventory_memo","メモ","","ここにメモを入力")
+            ?>
             <button type="submit" class="btn btn-primary">保存</button>
         </form>
     </div>
--- env.php
+++ env.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING &~E_DEPRECATED);
 #共通処理&環境変数
 #
 #
@@ -9,22 +9,47 @@ date_default_timezone_set('Asia/Tokyo');
 define("APP_NAME","Inventory");
 define("DB_FILE","inventory.db");
 
-#for dynamic navbar
-$inventory_navbar['0']['script'] = "index.php";
-$inventory_navbar['0']['name'] = "Home";
-$inventory_navbar['1']['script'] = "edit.php";
-$inventory_navbar['1']['name'] = "物品登録・編集";
-$inventory_navbar['2']['script'] = "list.php";
-$inventory_navbar['2']['name'] = "物品一覧";
+#pages
+$inventory_pages[0]['script'] = "index.php";
+$inventory_pages[0]['name'] = "Home";
+$inventory_pages[1]['script'] = "edit.php";
+$inventory_pages[1]['name'] = "物品登録・編集";
+$inventory_pages[2]['script'] = "list.php";
+$inventory_pages[2]['name'] = "物品一覧";
 
+#isfixed
+$inventory_isfixed[0][0] = 0;
+$inventory_isfixed[0][1] = "動産";
+$inventory_isfixed[1][0]= 1;
+$inventory_isfixed[1][1]= "固定資産";
 
+#message
+define("MSG_ASSET_RENEW_COMPLETE",'<div class="alert alert-success" role="alert">物品情報を更新しました。</div>');
+define("MSG_ASSET_NOT_FOUND",'<div class="alert alert-danger" role="alert">指定された物品IDは存在しません。</div>');
 
-#common
+#--------------------------common--------------------------
+
+#--------------------------maketitle--------------------------
+function generate_html_header($script_name){
+    print '<!DOCTYPE HTML>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    ';
+
+    generate_title($script_name);
+
+    print '</head>
+<body>';
+    generate_navbar($script_name);
+}
 
 function generate_title($script_name){
-    global $inventory_navbar;
+    global $inventory_pages;
     $page_title = "";
-    foreach($inventory_navbar as $nav_item){
+    foreach($inventory_pages as $nav_item){
         if($script_name == $nav_item["script"]){
             $page_title = $nav_item["name"];
         }
@@ -34,9 +59,9 @@ function generate_title($script_name){
     print '<title>'.$page_title.' - '.APP_NAME.'</title>';
 }
 
-#dynamic~~chord~~ navbar
+
 function generate_navbar($script_name){
-    global $inventory_navbar;
+    global $inventory_pages;
 
     print '<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">'.APP_NAME.'</a>
@@ -46,7 +71,7 @@ function generate_navbar($script_name){
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">';
 
-    foreach($inventory_navbar as $nav_item){
+    foreach($inventory_pages as $nav_item){
         if($script_name == $nav_item["script"]){
             $nav_item_class = 'nav-item active';
         }else{
@@ -59,5 +84,54 @@ function generate_navbar($script_name){
 
 
     print '</ul></div></nav>';
+
+}
+
+#--------------------------form parts maker--------------------------
+
+function forms_combobox($tag_id,$label,$itemarray,$seleteced = null){
+    print '<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>
+                    <select class="custom-select" id="'.$tag_id.'">';
+    foreach($itemarray as $item){
+        print '<option value="'.$item[0].'">'.$item[1].'</option>';
+    }
+    print '</select></div>';
+}
+
+
+function forms_textbox($tag_id,$label,$value = "",$placeholder = "",$readonly = false){
+
+    if($readonly){
+        $tag_option = "readonly";
+    }
+
+    print '<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>';
+    print '<input type="text" class="form-control" id="'.$tag_id.'" placeholder="'.$placeholder.'" value="'.$value.'" '.$tag_option.'></div>';
+
+}
+
+function forms_textarea($tag_id,$label,$value="",$placeholder = "",$readonly = false){
+    if($readonly){
+        $tag_option = "readonly";
+    }
+
+    print '<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>';
+    print '<textarea class="form-control" id="'.$tag_id.'" placeholder="'.$placeholder.'" value="'.$value.'" '.$tag_option.'></textarea></div>';
+
+}
+
+#-----------------------------database------------------------------
+function db_to_array($db_input){
+    if($db_input){
+        $i = 0;
+        while($toarraytmp = $db_input->fetchArray()){
+            $db_data[$i] = $toarraytmp;
+            $i++;
+        }
+        return $db_data;
+    }else{
+        return false;
+    }
+
 
 }
