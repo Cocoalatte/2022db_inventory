9a9c534d40ee9f71191b74e0d140fec35d957da0
Cocoalatte <github@chitose-tech.com>
2022-12-20 22:10:45 +0900

    add csrf checker and define database object in env.php

--- env.php
+++ env.php
@@ -1,6 +1,7 @@
 <?php
+#talisman
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING &~E_DEPRECATED);
-#共通処理&環境変数
+#
 #
 #
 #timezone
@@ -30,23 +31,23 @@ define("MSG_NEW_ASSET",'<div class="alert alert-info" role="alert">物品を新�
 #--------------------------common--------------------------
 
 #--------------------------maketitle--------------------------
-function generate_html_header($script_name){
-    print '<!DOCTYPE HTML>
+function maketitle($script_name):void{
+    print ('<!DOCTYPE HTML>
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    ';
+    ');
 
     generate_title($script_name);
 
-    print '</head>
-<body>';
+    print ('</head><body>');
     generate_navbar($script_name);
 }
 
-function generate_title($script_name){
+
+function generate_title($script_name):void{
     global $inventory_pages;
     $page_title = "";
     foreach($inventory_pages as $nav_item){
@@ -56,20 +57,20 @@ function generate_title($script_name){
 
     }
 
-    print '<title>'.$page_title.' - '.APP_NAME.'</title>';
+    print ('<title>'.$page_title.' - '.APP_NAME.'</title>');
 }
 
 
-function generate_navbar($script_name){
+function generate_navbar($script_name): void{
     global $inventory_pages;
 
-    print '<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    print ('<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">'.APP_NAME.'</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">';
+        <ul class="navbar-nav mr-auto">');
 
     foreach($inventory_pages as $nav_item){
         if($script_name == $nav_item["script"]){
@@ -77,59 +78,76 @@ function generate_navbar($script_name){
         }else{
             $nav_item_class = 'nav-item';
         }
-        print '<li class="'.$nav_item_class.'">
+        print ('<li class="'.$nav_item_class.'">
                 <a class="nav-link" href="'.$nav_item["script"].'">'.$nav_item["name"].'</a>
-            </li>';
+            </li>');
     }
 
 
-    print '</ul></div></nav>';
+    print ('</ul></div></nav>');
 
+}
+
+function makefooter():void{
+    print ('</body></html>');
 }
 
 #--------------------------form parts maker--------------------------
 
-function forms_combobox($tag_id,$label,$itemarray,$seleteced = null){
-    print '<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>
-                    <select class="custom-select" id="'.$tag_id.'">';
+function forms_combobox($tag_id,$label,$itemarray,$seleteced = null):void{
+    print ('<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>
+                    <select name="'.$tag_id.'" class="custom-select" id="'.$tag_id.'">');
     foreach($itemarray as $item){
-        print '<option value="'.$item[0].'">'.$item[1].'</option>';
+        print ('<option value="'.$item[0].'">'.$item[1].'</option>');
     }
-    print '</select></div>';
+    print ('</select></div>');
 }
 
 
-function forms_textbox($tag_id,$label,$value = "",$placeholder = "",$readonly = false){
+function forms_textbox($tag_id,$label,$value = "",$placeholder = "",$readonly = false):void{
 
     if($readonly){
         $tag_option = "readonly";
     }
 
-    print '<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>';
-    print '<input type="text" class="form-control" id="'.$tag_id.'" placeholder="'.$placeholder.'" value="'.$value.'" '.$tag_option.'></div>';
+    print ('<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>');
+    print ('<input type="text" name="'.$tag_id.'" class="form-control" id="'.$tag_id.'" placeholder="'.$placeholder.'" value="'.$value.'" '.$tag_option.'></div>');
 
 }
 
-function forms_textarea($tag_id,$label,$value="",$placeholder = "",$readonly = false){
+function forms_textarea($tag_id,$label,$value="",$placeholder = "",$readonly = false):void{
     if($readonly){
         $tag_option = "readonly";
     }
 
-    print '<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>';
-    print '<textarea class="form-control" id="'.$tag_id.'" placeholder="'.$placeholder.'" value="'.$value.'" '.$tag_option.'></textarea></div>';
+    print ('<div class="form-group"><label for="'.$tag_id.'" class="col-form-label">'.$label.'</label>');
+    print ('<textarea class="form-control" name="'.$tag_id.'" id="'.$tag_id.'" placeholder="'.$placeholder.'" value="'.$value.'" '.$tag_option.'></textarea></div>');
 
 }
 
-function forms_submit($label,$disabled=false){
+function forms_hidden($tag_id,$value = ""):void{
+    print ('<input type="hidden" name="'.$tag_id.'" value="'.$value.'">');
+}
+
+
+function forms_submit($label,$disabled=false): void{
     if($disabled){
-        print '<button type="submit" class="btn btn-secondary" disabled>'.$label.'</button>';
+        print ('<button type="submit" class="btn btn-secondary" disabled>'.$label.'</button>');
     }else{
-        print '<button type="submit" class="btn btn-primary">'.$label.'</button>';
+        print ('<button type="submit" class="btn btn-primary">'.$label.'</button>');
     }
 }
 
-#-----------------------------database------------------------------
-function db_to_array($db_input){
+#-----------------------------database-----------------------------
+#cocoroconect
+$dbhandle = new SQLite3(DB_FILE);
+
+#to support PDO in the future
+function query_to_array($dbhandle_,$sql): bool|array{
+    return db_to_array($dbhandle_->query($sql));
+}
+
+function db_to_array($db_input): bool|array{
     if($db_input){
         $i = 0;
         while($toarraytmp = $db_input->fetchArray()){
@@ -142,4 +160,24 @@ function db_to_array($db_input){
     }
 
 
+}
+
+#CSRF
+
+function generate_csrf_param(): string{
+    global $dbhandle;
+    $chkhash = hash("sha256",uniqid("",true));
+
+    $dbhandle->querySingle("INSERT INTO inventory_csrf_chk VALUES ('".$chkhash."',0);");
+    return $chkhash;
+}
+
+function csrf_make_used($param):void{
+    global $dbhandle;
+    $dbhandle->querySingle("UPDATE inventory_csrf_chk SET inventory_csrf_used = 1 WHERE inventory_csrf_hash ='".$param."';");
+}
+
+function csrf_is_used($param):bool{
+    global $dbhandle;
+    $result = $dbhandle->querySingle("SELECT inventory_csrf_used FROM inventory_csrf_chk WHERE inventory_csrf_hash = '".$param."';");
 }
