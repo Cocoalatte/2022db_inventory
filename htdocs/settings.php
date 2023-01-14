2e2bad02a2827eeacee823f64cc05dc3afc13279
Cocoalatte <github@chitose-tech.com>
2023-01-14 20:53:49 +0900

    add setting ui

--- htdocs/settings.php
+++ htdocs/settings.php
@@ -1,1 +1,19 @@
-<?php
+<?php require("core.php");
+
+
+#maketitle
+maketitle(basename(__FILE__));
+
+?>
+
+    <div class="container">
+        <h1 class="page-header">各種設定</h1><hr>
+        <h2>カテゴリ設定</h2>
+
+        <h2>予算区分設定</h2>
+
+        <h2>資産区分設定</h2>
+
+    </div>
+
+<?php makefooter() ?>
